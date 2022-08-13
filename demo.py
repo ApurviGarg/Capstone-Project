f8d26177c3bc62887cca19c92615e64071b017a8
Pramit Deep Kaur Gogna <65402284+Pramit29@users.noreply.github.com>
2022-08-14 00:27:20 +0530

    completed demo file

--- demo.py
+++ demo.py
@@ -15,7 +15,22 @@ min_height_rect = 80
 # This algo substract the background from cars in the video because we want to detect cars only
 algo = cv2.bgsegm.createBackgroundSubtractorMOG() # algo object gets created of algorithm:- Substractor
 
+# locate center of rectangle
+def center_handle(x,y,w,h):
+    x1 = int(w/2)
+    y1 = int(h/2)
+    cx = x+x1
+    cy = y+y1
+    return cx,cy
 
+# detects the vehicle and appends in this list
+detect = []
+
+# allowable error between pixel
+offset = 6
+
+# to count vehicles
+counter =0
 
 
 while True:
@@ -59,11 +74,26 @@ while True:
             continue
 
         cv2.rectangle(frame1,(x,y),(x+w,y+h),(0,255,0),2)
+        cv2.putText(frame1, "Vehicle"+str(counter),(x,y-20),cv2.FONT_HERSHEY_TRIPLEX,1,(255,200,0),2)
 
+        center = center_handle(x,y,w,h)
+        detect.append(center)
+        cv2.circle(frame1,center,4,(0,0,255),-1)
+        
+        # count vehicles
+        for(x,y) in detect:
+            if y<(count_line_position+offset) and y>(count_line_position-offset):
+                counter+=1
+                # when vehicle is detected colour of line changes
+                cv2.line(frame1,(25,count_line_position),(1200,count_line_position),(0,136,255),3)
+                # now remove the vehicle detected 
+                detect.remove((x,y))
+                print("vehicle counter:"+str(counter))
     
-
-
-
+    
+    # Writing on window
+    cv2.putText(frame1, " VEHICLE COUNTER : "+str(counter),(450,70),cv2.FONT_HERSHEY_SIMPLEX,2,(0,0,255),5)
+   
     cv2.imshow('Video Original',frame1)
 
     # If we press enter then the window will close (13 represents Enter key)
