50a7a0a203c012d87209270a1e4b748ea17eecbd
Pramit Deep Kaur Gogna <65402284+Pramit29@users.noreply.github.com>
2022-08-13 14:57:17 +0530

    initial code

---
+++ demo.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+
+# Web camera
+cap = cv2.VideoCapture('res3_video.mp4')
+
+# Initialize Substructor
+
+
+
+
+while True:
+    ret,frame1 = cap.read()
+    cv2.imshow('Video Original',frame1)
+
+    # If we press enter then the window will close
+    if cv2.waitKey(1)==13:
+        break
+
+# This is used to close the window
+cv2.destroyAllWindows()
+# This is used to release the video of traffic
+cap.release()
